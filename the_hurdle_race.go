f6d6321c27f606b5dd9db4b10d341573b14fb659
Aldo <38758944+AldoFusterTurpin@users.noreply.github.com>
2021-07-24 13:16:07 +0200

    Create the_hurdle_race.go

---
+++ the_hurdle_race.go
@@ -0,0 +1,96 @@
+// ALDO FUSTER TURPIN
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// 1 <= n,k <= 100
+// 1 <= height[i] <= 100
+func maxHeight(height []int32) int32 {
+    maxHeight := height[0]
+    for i := 1; i < len(height); i++ {
+        v := height[i]
+        if v > maxHeight {
+            maxHeight = v
+        }
+    }
+    return maxHeight
+}
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+func hurdleRace(k int32, height []int32) int32 {
+    maxHeight := maxHeight(height)
+    if k >= maxHeight {
+        return 0
+    } 
+    
+    return maxHeight - k
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+    checkError(err)
+    k := int32(kTemp)
+
+    heightTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var height []int32
+
+    for i := 0; i < int(n); i++ {
+        heightItemTemp, err := strconv.ParseInt(heightTemp[i], 10, 64)
+        checkError(err)
+        heightItem := int32(heightItemTemp)
+        height = append(height, heightItem)
+    }
+
+    result := hurdleRace(k, height)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
