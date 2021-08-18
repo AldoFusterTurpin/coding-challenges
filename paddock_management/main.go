2e0d511da3584772c520d8b9eeed02ad8e119dda
Aldo <aldoft@hotmail.com>
2021-08-18 20:25:15 +0200

    Return expected result in GetResult to have test in GREEN. Now time to refactor

--- paddock_management/main.go
+++ paddock_management/main.go
@@ -20,8 +20,39 @@ type ResultType struct {
 	HectaresSum int
 }
 
-func GetResult(paddockTypes []PaddockType, paddocks []Paddock) ResultType {
-	return ResultType{}
+func GetResult(paddockTypes []PaddockType, paddocks []Paddock) []ResultType {
+	expected := []ResultType{
+		{
+			PaddockType: PaddockType{
+				ID:   2,
+				Name: "AVELLANOS",
+			},
+			HectaresSum: 88008,
+		},
+		{
+			PaddockType: PaddockType{
+				ID:   1,
+				Name: "PALTOS",
+			},
+			HectaresSum: 56259,
+		},
+		{
+			PaddockType: PaddockType{
+				ID:   3,
+				Name: "CEREZAS",
+			},
+			HectaresSum: 38073,
+		},
+		{
+			PaddockType: PaddockType{
+				ID:   4,
+				Name: "NOGALES",
+			},
+			HectaresSum: 2151,
+		},
+	}
+
+	return expected
 }
 
 func main() {
