d26a87bc5365aa42920bdf92c0c4f7e2e455ed7a
Aldo <aldoft@hotmail.com>
2021-08-18 14:58:41 +0200

    Move struct types to main.go and create GetResult returning empty result. Now compiles but test is RED, good for now. Making progress step by step :)

--- paddock_management/main.go
+++ paddock_management/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type PaddockType struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Paddock struct {
+	PaddockManagerID int `json:"paddockManagerId"`
+	FarmID           int `json:"farmId"`
+	PaddockTypeID    int `json:"paddockTypeId"`
+	HarvestYear      int `json:"harvestYear"`
+	Area             int `json:"area"`
+}
+
+type ResultType struct {
+	PaddockType
+	HectaresSum int
+}
+
+func GetResult(paddockTypes []PaddockType, paddocks []Paddock) ResultType {
+	return ResultType{}
+}
+
 func main() {
 	fmt.Println("Main works")
 }
--- paddock_management/main_test.go
+++ paddock_management/main_test.go
@@ -5,24 +5,6 @@ import (
 	"testing"
 )
 
-type PaddockType struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-type Paddock struct {
-	PaddockManagerID int `json:"paddockManagerId"`
-	FarmID           int `json:"farmId"`
-	PaddockTypeID    int `json:"paddockTypeId"`
-	HarvestYear      int `json:"harvestYear"`
-	Area             int `json:"area"`
-}
-
-type ResultType struct {
-	PaddockType
-	HectaresSum int
-}
-
 func TestPaddockManager(t *testing.T) {
 	tt := map[string]struct {
 		paddockTypes []PaddockType
