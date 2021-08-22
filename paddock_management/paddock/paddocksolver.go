88569ade8c0c05cfbc9283693a67d1385ebd5524
Aldo <aldoft@hotmail.com>
2021-08-22 13:55:21 +0200

    Simplify comment

--- paddock_management/paddock/paddocksolver.go
+++ paddock_management/paddock/paddocksolver.go
@@ -24,14 +24,13 @@ func (s SimplePaddockSolver) SolveProblem(paddockTypes []PaddockType, paddocks [
 }
 
 // Returns a map where:
-// key is id of paddockType
+// key is id of paddockType,
 // value is sum of hectares of all paddocks that have as paddockTypeId the key of this map entry.
 // i.e: mp[i] returns the sum of hectares of all the paddocks that have "i" as paddockTypeId.
 func (s SimplePaddockSolver) getSumOfHectares(paddocks []Paddock) map[int]int {
 	mp := make(map[int]int)
 	for _, paddock := range paddocks {
-		// Don't need to check if entry exists. If the requested key doesn't exist,
-		// we get the value type's zero value.
+		// If the requested key doesn't exist, we get the value type's zero value.
 		mp[paddock.PaddockTypeID] += paddock.Area
 	}
 	return mp
